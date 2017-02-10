44bdf485a71169decb62d0be0dfa59fd710ec679
clabuzze <clabuzze@iastate.edu>
2017-02-09 21:35:16 -0600

    Add neonic_plant_samples documentation

---
+++ R/neonic_plant_samples.R
@@ -0,0 +1,21 @@
+#' Neonic plant samples
+#' 
+#' Contains neonic data from plant samples taken
+#' at various STRIPS1 experiment sites
+#' 
+#' @format A data.frame with the following columns:
+#'   \code{sample}: the unique plant sample identifier
+#'   \code{site}: the site where the sample originated
+#'   \code{date}: the date the sample was extracted
+#'   \code{plant_species}: species of sample
+#'   \code{plant_number}: plant identifier
+#'   \code{mass}: mass of sample in grams
+#'   \code{acetamiprid}: ng/g acetamiprid detected
+#'   \code{clothianidin}: ng/g clothianidin detected
+#'   \code{dinotefuran}: ng/g dinotefuran detected
+#'   \code{imidacloprid}: ng/g imidacloprid detected
+#'   \code{thiacloprid}: ng/g thiacloprid detected
+#'   \code{thiamethoxam}: ng/g thiamethoxam detected
+#'   \code{surrogate_percent_recovery_imidacloprid_d4}: percent recovery imidacloprid
+#'   
+"neonic_plant_sample"
