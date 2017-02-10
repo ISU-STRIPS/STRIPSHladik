a9ca7e9617daed00cc6f6293cee9c39cc2417a05
clabuzze <clabuzze@iastate.edu>
2017-02-09 21:18:44 -0600

    Tidy neonic_water_samples data

--- data-raw/neonic_water_samples.R
+++ data-raw/neonic_water_samples.R
@@ -1,4 +1,23 @@
 library(dplyr)
 
-neonic_water_samples <- readr::read_csv("neonic_water_samples.csv")
+neonic_water_samples <- readr::read_csv("neonic_water_samples.csv") %>%
+  
+  #Capitalize first letter of sites
+  mutate(site = Hmisc::capitalize(site)) %>%
+  
+  #Format dates (if no day provided, "01" inserted)
+  mutate(date_start = ifelse(
+    nchar(as.character(date_start)) == 6,
+    paste(as.character(date_start), "01", sep=""), 
+    as.character(date_start))
+  ) %>%
+  mutate(date_start = as.Date(date_start, "%Y%m%d")) %>%
+  mutate(date_end = as.Date(as.character(date_end), "%Y%m%d")) %>%
+  
+  #remove units from column names (in Readme)
+  rename(vol = vol_L) %>%
+  
+  #update surrogate_percentrecovery_imidacloprid_d4 column name
+  rename(surrogate_percent_recovery_imidacloprid_d4 = surrogate_percentrecovery_imidacloprid_d4)
+
 devtools::use_data(neonic_water_samples)
