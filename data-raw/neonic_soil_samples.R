73c39d242c80bbc103874c9d4d4023460458336f
clabuzze <clabuzze@iastate.edu>
2017-02-09 21:05:45 -0600

    Tidy neonic_soil_samples data

--- data-raw/neonic_soil_samples.R
+++ data-raw/neonic_soil_samples.R
@@ -1,4 +1,17 @@
 library(dplyr)
 
-neonic_soil_samples <- readr::read_csv("neonic_soil_samples.csv")
+neonic_soil_samples <- readr::read_csv("neonic_soil_samples.csv") %>%
+  
+  #Capitalize first letter of sites
+  mutate(site = capitalize(site)) %>%
+  
+  #Format dates
+  mutate(date = as.Date(as.character(date), "%Y%m%d")) %>%
+  
+  #remove units from column names (in Readme)
+  rename(mass = mass_g, vol = vol_L) %>%
+  
+  #update surrogate_percentrecovery_imidacloprid_d4 column name
+  rename(surrogate_percent_recovery_imidacloprid_d4 = surrogate_percentrecovery_imidacloprid_d4)
+
 devtools::use_data(neonic_soil_samples)
