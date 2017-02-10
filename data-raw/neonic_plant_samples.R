89bd66bce2a2b5ea2096aec5cc8e2608ae49951a
clabuzze <clabuzze@iastate.edu>
2017-02-09 21:00:50 -0600

    Tidy data in neonic_plant_samples

--- data-raw/neonic_plant_samples.R
+++ data-raw/neonic_plant_samples.R
@@ -1,4 +1,18 @@
 library(dplyr)
+library(Hmisc)
 
-neonic_plant_samples <- readr::read_csv("neonic_plant_samples.csv")
+neonic_plant_samples <- readr::read_csv("neonic_plant_samples.csv") %>%
+  
+  #Capitalize first letter of sites
+  mutate(site = capitalize(site)) %>%
+
+  #Format dates
+  mutate(date = as.Date(as.character(date), "%Y%m%d")) %>%
+  
+  #remove unit from mass_g column
+  rename(mass = mass_g) %>%
+
+  #update surrogate_percentrecovery_imidacloprid_d4 column name
+  rename(surrogate_percent_recovery_imidacloprid_d4 = surrogate_percentrecovery_imidacloprid_d4)
+
 devtools::use_data(neonic_plant_samples)
