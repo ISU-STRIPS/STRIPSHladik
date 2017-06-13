9244b0fed254f69bf4499f7baad94666c036caee
clabuzze <clabuzze@iastate.edu>
2017-06-13 11:37:21 -0500

    Update documentation formatting

--- R/STRIPSHladik.R
+++ R/STRIPSHladik.R
@@ -1,18 +1,25 @@
 #' STRIPSHladik: A package for STRIPS neonic data.
-#'
-#' The STRIPSHladik package contains STRIPS neonic data
-#' from plant, soil, and water samples
-#'
+#' 
+#' The STRIPSHladik package contains STRIPS neonic data from plant, soil, and
+#' water samples
+#' 
 #' @section STRIPSHladik data sets:
+#'   
+#'   \code{\link{neonic_plant_samples}}
+#'   
+#'   \code{\link{neonic_soil_samples}}
+#'   
+#'   \code{\link{neonic_water_samples}}
+#'   
+#'   
+#' @seealso
+#' 
 #' \code{\link{neonic_plant_samples}}
+#' 
 #' \code{\link{neonic_soil_samples}}
+#' 
 #' \code{\link{neonic_water_samples}}
-#'
-#' @seealso 
-#' \code{\link{neonic_plant_samples}} 
-#' \code{\link{neonic_soil_samples}}
-#' \code{\link{neonic_water_samples}}
-#'
+#' 
 #' @docType package
 #' @name STRIPSHladik
 NULL
--- R/neonic_soil_samples.R
+++ R/neonic_soil_samples.R
@@ -1,23 +1,39 @@
 #' Neonic soil samples
 #' 
-#' Contains neonic data from soil samples taken
-#' at various STRIPS1 experiment sites
+#' Contains neonic data from soil samples taken at various STRIPS1 experiment
+#' sites
 #' 
 #' @format A data.frame with the following columns:
+#'   
 #'   \code{sample}: the unique soil sample identifier
+#'   
 #'   \code{site}: the site where the sample originated
+#'   
 #'   \code{date}: the date the sample was extracted
+#'   
 #'   \code{type}: type of sample (see Readme)
+#'   
 #'   \code{material}: substrate of soil sample
+#'   
 #'   \code{mass}: mass of sample in grams
+#'   
 #'   \code{vol}: volume of sample in liters
+#'   
 #'   \code{organic_carbon_percent}: percent organic carbon
+#'   
 #'   \code{acetamiprid}: ng/g acetamiprid detected
+#'   
 #'   \code{clothianidin}: ng/g clothianidin detected
+#'   
 #'   \code{dinotefuran}: ng/g dinotefuran detected
+#'   
 #'   \code{imidacloprid}: ng/g imidacloprid detected
+#'   
 #'   \code{thiacloprid}: ng/g thiacloprid detected
+#'   
 #'   \code{thiamethoxam}: ng/g thiamethoxam detected
-#'   \code{surrogate_percent_recovery_imidacloprid_d4}: percent recovery imidacloprid
+#'   
+#'   \code{surrogate_percent_recovery_imidacloprid_d4}: percent recovery
+#'   imidacloprid
 #'   
 "neonic_soil_samples"
